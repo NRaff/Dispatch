c8a46262d86307b0e47c567fb8ec9cf6881910e1
nraff <nraff510@gmail.com>
2021-11-17 17:30:57 -0800

    add app controller auth funcs

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = user.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login!(@user)
+      # render :show
+    else
+      flash.now[:errors] = ["Invalid username or password"]
+      # render :new
+    end
+  end
+
+  def destroy
+
+  end
+end
