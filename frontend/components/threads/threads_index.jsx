fd8c18a61a6dad09c21caa32fe9e6343e4a46513
nraff <nraff510@gmail.com>
2021-11-28 11:19:33 -0800

    refacter threads_index to class component

--- frontend/components/threads/threads_index.jsx
+++ frontend/components/threads/threads_index.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import NewThread from "./new_thread";
 import ThreadItem from "./thread_item";
 
-const ThreadsIndex = ({threads, createThread, deleteThread, setActiveThread, createSocket}) => {
-  return (
-    <div className='all-threads'>
-      <NewThread createThread={createThread} />
-      {
-        threads.map(item => (
-          <ThreadItem
-            key={item.id}
-            thread={item}
-            deleteThread={() => deleteThread(item.id)}
-            setActiveThread={() => setActiveThread(item.id)}
-            createSocket={() => createSocket(item.id)}
-          />
-        ))
-      }
-    </div>
-  )
+class ThreadsIndex extends React.Component{
+  componentDidMount(){
+    // do user_config channel set up here
+  }
+
+  render(){
+    const { 
+      threads,
+      createThread, 
+      deleteThread, 
+      setActiveThread, 
+      createSocket 
+    } = this.props
+    return (
+      <div className='all-threads'>
+        <NewThread createThread={createThread} />
+        {
+          threads.map(item => (
+            <ThreadItem
+              key={item.id}
+              thread={item}
+              deleteThread={() => deleteThread(item.id)}
+              setActiveThread={() => setActiveThread(item.id)}
+              createSocket={() => createSocket(item.id)}
+            />
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default ThreadsIndex;
