832274b81b4618b573b0bd3f9dd7916665cccd8d
nraff <nraff510@gmail.com>
2021-11-29 18:06:22 -0800

    complete threads and dms (untested)

--- frontend/components/threads/threads_index.jsx
+++ frontend/components/threads/threads_index.jsx
@@ -4,6 +4,43 @@ import ThreadItem from "./thread_item";
 import * as RealtimeUser from "../../utils/user_config_socket"
 
 class ThreadsIndex extends React.Component{
+  renderThreads(){
+    const {setActiveThread, createSocket} = this.props
+    const allThreads = this.props.threads
+    const threads = allThreads.filter(thread => thread.isThread)
+    return (
+      <div className='threads'>
+        {threads.map(item => (
+          <ThreadItem
+            key={item.id}
+            thread={item}
+            deleteThread={() => RealtimeUser.deleteThread({ thread: item.id, user: this.props.currentUserId })}
+            setActiveThread={() => setActiveThread(item.id)}
+            createSocket={() => createSocket(item.id)}
+          />
+        ))}
+      </div>
+    )
+  }
+
+  renderDMs() {
+    const { setActiveThread, createSocket } = this.props
+    const allThreads = this.props.threads
+    const dms = allThreads.filter(thread => !thread.isThread)
+    return (
+      <div className='dms'>
+        {dms.map(item => (
+          <ThreadItem
+            key={item.id}
+            thread={item}
+            deleteThread={() => RealtimeUser.deleteThread({ thread: item.id, user: this.props.currentUserId })}
+            setActiveThread={() => setActiveThread(item.id)}
+            createSocket={() => createSocket(item.id)}
+          />
+        ))}
+      </div>
+    )
+  }
   render(){
     const { 
       threads,
@@ -15,7 +52,9 @@ class ThreadsIndex extends React.Component{
         <NewThread 
           currentUser={this.props.currentUserId}
         />
-        {
+        {this.renderThreads()}
+        {this.renderDMs()}
+        {/* {
           threads.map(item => (
             <ThreadItem
               key={item.id}
@@ -25,7 +64,7 @@ class ThreadsIndex extends React.Component{
               createSocket={() => createSocket(item.id)}
             />
           ))
-        }
+        } */}
       </div>
     )
   }
