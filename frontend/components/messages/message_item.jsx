91620225483b5ae1da4df7738a5a01f2ca066c1a
nraff <nraff510@gmail.com>
2021-12-01 10:24:37 -0800

    update message item to class object

--- frontend/components/messages/message_item.jsx
+++ frontend/components/messages/message_item.jsx
@@ -3,35 +3,52 @@ import { Link } from "react-router-dom";
 import { Icons } from "../../utils/theme";
 import * as RealtimeThread from '../../utils/thread_chat_socket'
 
-const createdTime = timestamp => {
-  let date = new Date(timestamp)
-  return date.toLocaleString()
-}
+class MessageItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.deleteMessage = this.deleteMessage.bind(this)
+  }
 
-const deleteMessage = (payload) => {
-  RealtimeThread.deleteMessage(payload)
-}
+  createdTime(timestamp) {
+    let date = new Date(timestamp)
+    return date.toLocaleString()
+  }
 
+  deleteMessage(payload) {
+    RealtimeThread.deleteMessage(payload)
+  }
 
-const MessageItem = ({message, sender, currentUserId, updateMessage, /*deleteMessage*/}) => (
-  <div className='full-message'>
-    <img className={Icons.profile.lightProfile} alt="profileImg" />
-    <div className='message-item'>
-      <div className='message-header'>
-        <h3>{sender}</h3>
-        <p>{createdTime(message.createdAt)}</p>
+  renderCurrentUserMessage() {
+
+  }
+
+  renderOtherUserMessage() {
+
+  }
+
+  render() {
+    const { message, sender, currentUserId } = this.props
+    return (
+      <div className='full-message'>
+        <img className={Icons.profile.lightProfile} alt="profileImg" />
+        <div className='message-item'>
+          <div className='message-header'>
+            <h3>{sender}</h3>
+            <p>{this.createdTime(message.createdAt)}</p>
+          </div>
+          <p>{message.message}</p>
+        </div>
+        <div className='edit-delete'>
+          <Link className='edit-btn' to={`/edit/${message.id}`}>Edit</Link>
+          <button
+            onClick={() => this.deleteMessage({ id: message.id, sender_id: currentUserId, threadId: message.threadId })}
+            className='delete-btn'
+          >Delete</button>
+        </div>
       </div>
-      <p>{message.message}</p>
-    </div>
-    <div className='edit-delete'>
-      <Link className='edit-btn' to={`/edit/${message.id}`}>Edit</Link>
-      <button 
-        onClick={() => deleteMessage({id: message.id, sender_id: currentUserId, threadId: message.threadId})}
-        className='delete-btn'
-      >Delete</button>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default MessageItem;
 
