c52d3683cbc42145d8d74fe04feb46b772966931
nraff <nraff510@gmail.com>
2021-12-03 09:12:43 -0800

    final commit pre merge to main

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Workspace.delete_all
-workspaces = Workspace.create([
-  {name: 'Tattooine'},
-  {name: 'Hoth'},
-  {name: 'Coruscant'}
-])
+require 'byebug'
 
 User.delete_all
 UserWorkspace.delete_all
-Workspace.first.users.create([
-  {password: 123456, display_name: 'nick', email: 'nick@email.com'},
-  {password: 123456, display_name: 'One', email: 'one@email.com'},
-  {password: 123456, display_name: 'Two', email: 'two@email.com'},
-  {password: 123456, display_name: 'Three', email: 'three@email.com'},
-  {password: 123456, display_name: 'Demo', email: 'Demo@email.com'},
-  {password: 123456, display_name: 'Free Guy', email: 'free@email.com'}
+# byebug
+users = User.create([
+  {password: 123456, display_name: 'Obi Wan', email: '01@email.com'},
+  {password: 123456, display_name: 'Quigon', email: 'q@email.com'},
+  {password: 123456, display_name: 'Mace Windu', email: 'mace@email.com'},
+  {password: 123456, display_name: 'Anakin Skywalker', email: 'skyguy@email.com'},
+  {password: 123456, display_name: 'Yoda', email: 'yoda@email.com'},
+  {password: 123456, display_name: 'Ashoka Tano', email: 'at@email.com'},
+  {password: 123456, display_name: 'Demo User', email: 'Demo@email.com'}
+
+])
+
+obi_wan = users.select { |u| u.display_name == 'Obi Wan'}[0]
+quigon = users.select { |u| u.display_name == 'Quigon'}[0]
+anakin = users.select { |u| u.display_name == 'Anakin Skywalker'}[0]
+yoda = users.select { |u| u.display_name == 'Yoda'}[0]
+ashoka = users.select { |u| u.display_name == 'Ashoka Tano'}[0]
+mace = users.select { |u| u.display_name == 'Mace Windu'}[0]
+
+Workspace.delete_all
+workspaces = Workspace.create([
+  {name: 'Tattooine', created_by: anakin.id },
+  {name: 'Hoth', created_by: ashoka.id},
+  {name: 'Coruscant', created_by: obi_wan.id}
 ])
 
 tattooine = Workspace.find_by(name: 'Tattooine')
@@ -32,22 +44,42 @@ ws_assignments = []
 users.each do |user|
   ws_assignments.push({user_id: user.id, workspace_id: hoth.id})
   ws_assignments.push({user_id: user.id, workspace_id: coruscant.id})
+  ws_assignments.push({user_id: user.id, workspace_id: tattooine.id})
+
 end
 
 UserWorkspace.create(ws_assignments)
 
+# these need workspace ids
+# some should be DMs
 MessageThread.delete_all
 threads = MessageThread.create([
-  {name: 'Thread 1'},
-  {name: 'Thread 2'},
-  {name: 'Thread 3'},
-  {name: 'Thread 4'},
+  {name: 'Cantina', workspace_id: tattooine.id, is_thread: true},
+  {name: 'Rebel Base', workspace_id: hoth.id, is_thread: true},
+  {name: 'Jedi Temple', workspace_id: coruscant.id, is_thread: true},
+  {name: 'The Senate', workspace_id: coruscant.id, is_thread: true},
 ])
+
+thread_assignments = []
+users.each do |user|
+  threads.each do |t|
+    thread_assignments.push({user_id: user.id, thread_id: t.id})
+  end
+end
+UserThread.create(thread_assignments)
+
+
+cantina = MessageThread.find_by(name: 'Cantina')
+rebel_base = MessageThread.find_by(name: 'Rebel Base')
+jedi_temple = MessageThread.find_by(name: 'Jedi Temple')
+senate = MessageThread.find_by(name: 'The Senate')
 
 Message.delete_all
 messages = Message.create([
-  {thread_id: 24, message: 'Hello world'},
-  {thread_id: 24, message: 'Hello world 2'},
-  {thread_id: 24, message: 'Hello world 3'},
-  {thread_id: 24, message: 'Hello world 4'}
+  {thread_id: cantina.id, message: 'Have you ever been to the races?', sender_id: anakin.id},
+  {thread_id: cantina.id, message: 'What races?', sender_id: quigon.id},
+  {thread_id: cantina.id, message: "Pod racing! It's the fastest sport in the galaxy!", sender_id: anakin.id},
+  {thread_id: cantina.id, message: "We should definitely bet everything on it.", sender_id: quigon.id},
+  {thread_id: rebel_base.id, message: "We weren't actually on hoth in the movie were we?", sender_id: mace.id},
+  {thread_id: rebel_base.id, message: "In the movie at all, we were not.", sender_id: yoda.id}
 ])
